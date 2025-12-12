f70239099e7e0c33b0d9d48d03e7d727df3f1fa1
skeyboy <lylapp@163.com>
2025-12-12 21:42:04 +0800

    模型补全

--- lib/bridge_client/response/torrent_details_response.dart
+++ lib/bridge_client/response/torrent_details_response.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_butailing/bridge_client/response/torrent_details_response_file.dart';
+import 'package:flutter_butailing/bridge_client/response/torrent_stats.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'torrent_details_response.freezed.dart';
 part 'torrent_details_response.g.dart';
@@ -7,10 +9,10 @@ abstract class TorrentDetailsResponse with _$TorrentDetailsResponse {
   const factory TorrentDetailsResponse({
     required int id,
     String? name,
-    @Default([]) List? files,
+    @Default([]) List<TorrentDetailsResponseFile>? files,
     @JsonKey(name: "info_hash") required String infoHash,
     @JsonKey(name: "output_folder") required String outputFolder,
-    @Default([]) List? stats,
+    @Default([]) List<TorrentStats>? stats,
   }) = _TorrentDetailsResponse;
 
   factory TorrentDetailsResponse.fromJson(Map<String, dynamic> json) =>
