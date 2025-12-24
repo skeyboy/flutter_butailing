6e49805baa45751f706017b3258e8ee2722df46d
skeyboy <lylapp@163.com>
2025-12-24 16:15:27 +0800

    Android编译问题

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -47,6 +48,10 @@ android {
         // when your project uses more than one plugin that depend on C++ libs.
         pickFirst("lib/**/libc++_shared.so")
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
